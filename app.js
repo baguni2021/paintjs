d4de4e5f4fce691e3cc5e3ce5b0d04b662742040
forid48@gmail.com <forid48@gmail.com>
2021-07-28 05:54:37 +0900

    2.6 Saving the Image

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.querySelector("#jsRange");
 const mode = document.querySelector("#jsMode");
+const saveBtn = document.querySelector("#jsSave");
 
 const INITIAL_COLOR = "#2c2c2c"
 const CANVAS_SIZE = 700;
@@ -10,6 +11,8 @@ const CANVAS_SIZE = 700;
 canvas.width = CANVAS_SIZE;
 canvas.height = CANVAS_SIZE;
 
+ctx.fillStyle = "white";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle = INITIAL_COLOR;
 ctx.lineWidth = 2.5;
@@ -67,12 +70,25 @@ function handleCanvasClick() {
     
 }
 
+function handleContextMenu(event) {
+    event.preventDefault();
+}
+
+function handleSaveClick() {
+    const image = canvas.toDataURL();
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "PaintJS[🎨]";
+    link.click();
+}
+
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove)
     canvas.addEventListener("mousedown", startPainting)
     canvas.addEventListener("mouseup", stopPainting)
     canvas.addEventListener("mouseleave", stopPainting)
     canvas.addEventListener("click", handleCanvasClick)
+    canvas.addEventListener("contextmenu", handleContextMenu)
 }
 
 Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick ))
@@ -81,7 +97,10 @@ if(range) {
     range.addEventListener("input", handleRangeChange)
 }
 
-//mode , handleModeClick, filling =false
 if(mode){
     mode.addEventListener("click", handleModeClick)
+}
+
+if(saveBtn){
+    saveBtn.addEventListener("click", handleSaveClick)
 }
