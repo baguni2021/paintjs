d158dd46f7ae67fb7de9b687c054b0851fc5ecc4
forid48@gmail.com <forid48@gmail.com>
2021-07-28 04:03:32 +0900

    2.3 Changing Color

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const canvas = document.querySelector("#jsCanvas");
 const ctx = canvas.getContext("2d");
 
+canvas.width = 700;
+canvas.height = 700;
+
 ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
 
@@ -18,11 +21,13 @@ function onMouseMove(event) {
     const x = event.offsetX
     const y = event.offsetY
     if(!painting) {
-        ctx.beginPath();
-        ctx.moveTo(x, y);
+        console.log("Creating Path in", x, y)
+        ctx.beginPath(); //경로 생성
+        ctx.moveTo(x, y); //선 시작 좌표
     } else {
-        ctx.lineTo(x, y)
-        ctx.stroke()
+        console.log("Creating Line in", x, y)
+        ctx.lineTo(x, y) //선 끝 좌표
+        ctx.stroke() //선 그리기
     }
 }
 
